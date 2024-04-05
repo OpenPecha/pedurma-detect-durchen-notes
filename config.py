88042bda8e88542d55d599c3a07b4b42cde1ebc6
test <ten13zin@gmail.com>
2024-04-05 13:04:19 +0530

    use template matching

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 from pathlib import Path
 
 IMAGES_PATH = Path(__file__).parent.parent / "images"
+TEMPLATES_PATH = Path(__file__).parent / "templates"
+TEMPLATES_PATH.mkdir(parents=True, exist_ok=True)
