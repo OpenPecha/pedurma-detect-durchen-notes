f5ae1207df447fa6e1c04cf29f0f7a98f6a2fb8a
test <ten13zin@gmail.com>
2024-04-12 13:12:54 +0530

    update template matching algo

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ from pathlib import Path
 IMAGES_PATH = Path(__file__).parent.parent / "images"
 TEMPLATES_PATH = Path(__file__).parent / "templates"
 TEMPLATES_PATH.mkdir(parents=True, exist_ok=True)
+YOLO_TRAINING_DATA_PATH = Path(__file__).parent / "yolo_training_data"
