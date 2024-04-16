000176f796ac5fe245c6cd8bb58ca8118c3818ff
test <ten13zin@gmail.com>
2024-04-16 13:19:48 +0530

    trained note_classifier model

--- config.py
+++ config.py
@@ -3,4 +3,10 @@ from pathlib import Path
 IMAGES_PATH = Path(__file__).parent.parent / "images"
 TEMPLATES_PATH = Path(__file__).parent / "templates"
 TEMPLATES_PATH.mkdir(parents=True, exist_ok=True)
-YOLO_TRAINING_DATA_PATH = Path(__file__).parent / "yolo_training_data"
+
+# Training data paths
+TRAINING_DATA_PATH = Path(__file__).parent / "traindata"
+NOTE_CLASSIFIER_TRAINING_DATA = TRAINING_DATA_PATH / "note_classifier"
+NOTE_CLASSIFIER_TRAINING_DATA.mkdir(parents=True, exist_ok=True)
+NOTE_DETECTION_TRAINING_DATA = TRAINING_DATA_PATH / "note_detection"
+NOTE_DETECTION_TRAINING_DATA.mkdir(parents=True, exist_ok=True)
