c43d8ccb4ff5017e6b921317ce176d4b80b45802
test <ten13zin@gmail.com>
2024-05-04 13:10:48 +0530

    created yolo training data v3

--- pecha.py
+++ pecha.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 from collections import defaultdict
 from pathlib import Path
@@ -22,7 +23,10 @@ class PechaService:
 
     def in_ranges(self, page_name: str, ranges: list[tuple[str, str]]) -> bool:
         for page_range in ranges:
-            if page_range[0] <= page_name <= page_range[1]:
+            if page_range[0] == page_range[1]:
+                if page_name == page_range[0]:
+                    return True
+            if page_range[0] <= page_name < page_range[1]:
                 return True
         return False
 
@@ -47,8 +51,50 @@ def convert_mapping(old_mapping_fn: Path):
     json.dump(new_mappings, new_mapping_fn.open("w"), indent=2)
 
 
+def classify_images(pecha_path: Path):
+    pecha_service = PechaService(pecha_path.name)
+    for vol_dir in pecha_path.iterdir():
+        if not vol_dir.is_dir():
+            continue
+        print("Classifying", vol_dir.name, "...")
+        text_pages_path = vol_dir / "text_pages"
+        text_pages_path.mkdir(exist_ok=True, parents=True)
+        note_pages_path = vol_dir / "note_pages"
+        note_pages_path.mkdir(exist_ok=True, parents=True)
+        for img_fn in (vol_dir / "images").iterdir():
+            if not img_fn.name.endswith(".jpg"):  # Skip non-image files
+                continue
+            img_name = img_fn.stem
+            try:
+                if pecha_service.is_text_page(img_name):
+                    img_fn.rename(text_pages_path / img_fn.name)
+                elif pecha_service.is_note_page(img_name):
+                    img_fn.rename(note_pages_path / img_fn.name)
+            except KeyError as e:
+                print("Volume not found in mapping", vol_dir.name)
+                print(e)
+                break
+
+
+def reverse_classify_images(pecha_path: Path):
+    for vol_dir in pecha_path.iterdir():
+        if not vol_dir.is_dir():
+            continue
+        print("Reverse Classifying", vol_dir.name, "...")
+        for img_fn in (vol_dir / "text_pages").iterdir():
+            if not img_fn.name.endswith(".jpg"):  # Skip non-image files
+                continue
+            img_fn.rename(vol_dir / "images" / img_fn.name)
+        for img_fn in (vol_dir / "note_pages").iterdir():
+            if not img_fn.name.endswith(".jpg"):  # Skip non-image files
+                continue
+            img_fn.rename(vol_dir / "images" / img_fn.name)
+
+
 if __name__ == "__main__":
-    pecha = PechaService("W1PD95844")
-    print(pecha.is_text_page("I1PD958470040"))
-    print(pecha.is_text_page("I1PD958470042"))
-    print(pecha.is_note_page("I1PD958470049"))
+    parser = argparse.ArgumentParser(description="Classify Images")
+    parser.add_argument("pecha_path", type=str, help="Pecha Path")
+    args = parser.parse_args()
+    pecha_path = Path(args.pecha_path)
+    # reverse_classify_images(pecha_path)
+    classify_images(pecha_path)
