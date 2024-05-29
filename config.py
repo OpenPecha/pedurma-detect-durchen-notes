9a68c850f38242d1aba05bd4718d0d883aff0477
test <ten13zin@gmail.com>
2024-05-29 10:20:01 +0530

    update

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
 from pathlib import Path
 
 IMAGES_PATH = Path(__file__).parent.parent / "images"
+INPUT_PATH = Path(__file__).parent / "input"
+OUTPUT_PATH = Path(__file__).parent / "output"
+INPUT_PATH.mkdir(parents=True, exist_ok=True)
+IMAGES_OUTPUT_PATH = OUTPUT_PATH / "images"
+OCR_OUTPUT_PATH = OUTPUT_PATH / "ocr"
+IMAGES_OUTPUT_PATH.mkdir(parents=True, exist_ok=True)
+OCR_OUTPUT_PATH.mkdir(parents=True, exist_ok=True)
 
 # Templates
 NOTE_MAKER_TEMPLATES_PATH = Path(__file__).parent / "templates" / "note_maker"
@@ -10,6 +17,8 @@ DOUBLE_TSEK_TEMPLATES_PATH.mkdir(parents=True, exist_ok=True)
 
 # Training data paths
 TRAINING_DATA_PATH = Path(__file__).parent / "traindata"
+
+# Note
 NOTE_CLASSIFIER_TRAINING_DATA = TRAINING_DATA_PATH / "note_classifier"
 NOTE_CLASSIFIER_TRAINING_DATA.mkdir(parents=True, exist_ok=True)
 NOTE_NUM_RECOGNITION_DATASET = TRAINING_DATA_PATH / "note_num_recogination"
@@ -17,8 +26,20 @@ NOTE_NUM_RECOGNITION_DATASET.mkdir(parents=True, exist_ok=True)
 NOTE_DETECTION_TRAINING_DATA = TRAINING_DATA_PATH / "note_detection"
 NOTE_DETECTION_TRAINING_DATA.mkdir(parents=True, exist_ok=True)
 
+# double tsek
+DOUBLE_TSEK_CLASSIFIER_TRAINING_DATA = TRAINING_DATA_PATH / "double_tsek_classifier"
+DOUBLE_TSEK_CLASSIFIER_TRAINING_DATA.mkdir(parents=True, exist_ok=True)
+DOUBLE_TSEK_DETECTION_TRAINING_DATA = TRAINING_DATA_PATH / "double_tsek_detection"
+DOUBLE_TSEK_DETECTION_TRAINING_DATA.mkdir(parents=True, exist_ok=True)
+
 # Models
-NOTE_CLASSIFIER_MODEL_PATH = Path(__file__).parent / "models" / "note_classifier" / "note_classifier.keras"
-NOTE_CLASSIFIER_MODEL_PATH = Path(__file__).parent / "models" / "note_num_recognition" / "note_num_recognition.keras"
-# NOTE_DETECTION_MODEL_PATH = Path(__file__).parent / "models" / "note_detection" / "yolov8s_train1_e10.pt"
-NOTE_DETECTION_MODEL_PATH = Path(__file__).parent / "models" / "note_detection" / "yolov8s_train_e2.pt"
+MODELS_PATH = Path(__file__).parent / "models"
+
+## Note Models
+NOTE_CLASSIFIER_MODEL_PATH = MODELS_PATH / "note_classifier" / "note_classifier.keras"
+NOTE_NUM_RECOGNIZER_MODEL_PATH = MODELS_PATH / "note_num_recognition" / "note_num_recognition.keras"
+NOTE_DETECTION_MODEL_PATH = MODELS_PATH / "note_detection" / "yolov8s_e20.pt"
+
+## Double-Tsek models
+DOUBLE_TSEK_CLASSIFIER_MODEL_PATH = MODELS_PATH / "double_tsek_classifier" / "double_tsek_classfier.keras"
+DOUBLE_TSEK_DETECTION_MODEL_PATH = MODELS_PATH / "double_tsek_detection" / "best.pt"
--- models.py
+++ models.py
@@ -1,9 +1,13 @@
 from pathlib import Path
+import os
 
 import keras
 from ultralytics import YOLO
+import numpy as np
 
 import config
+
+os.environ["KERAS_BACKEND"] = "tensorflow"
 
 class NoteClassifier:
 
@@ -20,24 +24,57 @@ class NoteClassifier:
         return score
 
 note_classifier = NoteClassifier(model_path=config.NOTE_CLASSIFIER_MODEL_PATH)
-        
 
-class NoteDetection:
+class DoubleTsekClassifier:
 
     def __init__(self, model_path: Path):
-        self.model = YOLO(model_path)
-        self.image_size = 1472
+        self.model = keras.saving.load_model(model_path)
+        self.image_size = (23, 15)
 
     def predict(self, image_path: Path):
-        result = self.model.predict(image_path, imgsz=self.image_size)
+        img = keras.utils.load_img(str(image_path), target_size=self.image_size)
+        img_array = keras.utils.img_to_array(img)
+        img_array = keras.ops.expand_dims(img_array, 0)  # Create batch axis
+        predictions = self.model.predict(img_array, verbose=0)
+        score = float(keras.ops.sigmoid(predictions[0][0]))
+        return score
+
+double_tsek_classifier = DoubleTsekClassifier(model_path=config.DOUBLE_TSEK_CLASSIFIER_MODEL_PATH)
+
+class YoloDetector:
+
+    def __init__(self, model_path: Path, image_size=1472):
+        self.model = YOLO(model_path)
+        self.image_size = image_size
+
+    def predict(self, image_path: Path, x_adjust=0, y_adjust=0):
+        result = self.model.predict(image_path, imgsz=self.image_size, verbose=False)
         orig_img_h, orig_img_w = result[0].orig_shape
-        print(result[0].orig_shape)
         for box in result[0].boxes:
             x_center, y_center, w, h = box.xywh[0]
-            x = int(round(float(x_center) - float(w) / 2, 0))
-            y = int(round(float(y_center) - float(h) / 2, 0))
+            x = int(round(float(x_center) - float(w) / 2, 0)) + x_adjust
+            y = int(round(float(y_center) - float(h) / 2, 0)) + y_adjust
             w = int(round(float(w), 0))
             h = int(round(float(h), 0))
             yield x, y, w, h
 
-note_detector = NoteDetection(model_path=config.NOTE_DETECTION_MODEL_PATH)
+note_detector = YoloDetector(model_path=config.NOTE_DETECTION_MODEL_PATH)
+double_tsek_detector = YoloDetector(model_path=config.DOUBLE_TSEK_DETECTION_MODEL_PATH)
+
+
+class NoteNumRecoginiser:
+
+    def __init__(self, model_path: Path):
+        self.model = keras.saving.load_model(model_path)
+        self.image_size = (20, 20)
+        self.class_names = ['1', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '2', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '3', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '4', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '5', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '6', '60', '61', '62', '64', '7', '8', '9']
+
+    def predit(self, image_path):
+        img = keras.utils.load_img(str(image_path), target_size=self.image_size)
+        img_array = keras.utils.img_to_array(img)
+        img_array = keras.ops.expand_dims(img_array, 0)  # Create batch axis
+        predictions = self.model.predict(img_array, verbose=0)
+        return self.class_names[np.argmax(predictions[0])]
+
+
+note_num_recogniser = NoteNumRecoginiser(model_path=config.NOTE_NUM_RECOGNIZER_MODEL_PATH)
